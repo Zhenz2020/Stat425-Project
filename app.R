1e3d58cc6bf582eba2f7bc461876aa227f62120f
Zhenz2020 <89534431+Zhenz2020@users.noreply.github.com>
2022-04-19 14:05:30 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -15,11 +15,99 @@ library(readr)
 library(mice)
 library(Hmisc)
 library(DT)
+library(forecast)
+library(splines)
+library(tidyverse)
+library(dbplyr)
+library(xgboost)
+library(shinydashboard)
 
+
+
+cluster_plot <- function(ts,myK){
+    set.seed(123)
+    length.out = ncol(ts)
+    x = seq(0, 1, length.out )
+    F=ns(1:length.out,df = 9, intercept = FALSE)
+    F=t(t(F) - colMeans(F))
+    B=solve(t(F)%*%F)%*%t(F)%*%t(ts)
+    B=t(B)
+    mykm1=kmeans(B,myK)
+    par(mfrow=c(ceiling(myK/3),3))
+    ts_temp=ts
+    ts_temp=cbind(ts_temp,as.matrix(rep(0,dim(ts_temp)[1]),dim(ts_temp)[1],1))
+    mycenters1=matrix(rep(0,myK*ncol(ts)),ncol=ncol(ts))
+    for(k in 1:myK){
+        id=which(mykm1$cluster==k)
+        plot(NA, xlim = c(1, ncol(ts)), ylim = range(ts), xlab = "days", ylab = "values")
+        B_i= B[id,]
+        temp_mycenters1= colMeans(B_i)
+        mycenters1[k,]=F %*% t(t(temp_mycenters1))
+        for(i in 1:length(id)){
+            lines(1:ncol(ts), ts[id[i],] , col="gray")
+        }
+        lines(1:ncol(ts), mycenters1[k,], col="red")
+    }
+}
+
+cluster_tstemp <- function(ts,myK){
+    set.seed(123)
+    length.out = ncol(ts)
+    x = seq(0, 1, length.out )
+    F=ns(1:length.out,df = 9, intercept = FALSE)
+    F=t(t(F) - colMeans(F))
+    B=solve(t(F)%*%F)%*%t(F)%*%t(ts)
+    B=t(B)
+    mykm1=kmeans(B,myK)
+    par(mfrow=c(ceiling(myK/3),3))
+    ts_temp=ts
+    ts_temp=cbind(ts_temp,as.matrix(rep(0,dim(ts_temp)[1]),dim(ts_temp)[1],1))
+    mycenters1=matrix(rep(0,myK*ncol(ts)),ncol=ncol(ts))
+    for(k in 1:myK){
+        id=which(mykm1$cluster==k)
+        B_i= B[id,]
+        temp_mycenters1= colMeans(B_i)
+        mycenters1[k,]=F %*% t(t(temp_mycenters1))
+        ts_temp[id,length.out+1]=k
+    }
+    return(ts_temp)
+}
+
+
+
+
+
+xgboost_pre <- function(new_data,ts_temp,days){
+    set.seed(123)
+    train_x=as.matrix(ts_temp[,1:days])
+    train_y=as.matrix(ts_temp[,32])
+    new_data=matrix(new_data[1:days,],nrow = 1)
+    xgb.model <- xgboost(data = train_x, 
+                         label = train_y, max_depth = 8,
+                         eta = 0.04, nrounds = 1000,
+                         subsample = 0.5,
+                         verbose = FALSE)
+    xgb.pred = predict(xgb.model, newdata = as.matrix(new_data))
+    xgb_pred=round(xgb.pred)
+    if (xgb_pred==1){
+        remark="This data set belongs to the first group. During this month, it will grow slowly and tend to converge in the later stages"
+    }else if(xgb_pred==2){
+        remark="This data set belongs to the second group. During this month, the number of listens to this song will rise evenly"
+    }
+    else if(xgb_pred==3){
+        remark="This data set belongs to the third group. In the later stages, listening drops"
+    }
+    else if(xgb_pred==4){
+        remark="This data set belongs to the fourth group. In the later stages, listening is unstable, but generally increases at a higher rate"
+    }
+    else{
+        remark="This data set belongs to the fifth group. It is expected to peak at around 25 days, but then decline"
+    }
+    return(remark)
+}
 #replace temp with name of dataset
 #days: how many days forward you want to predict
 #conflow,confhigh: confidence interval for prediction
-library(forecast)
 #temp=read_csv("https://raw.githubusercontent.com/Zhenz2020/Stat427-Project/main/New_GeniusPageView.csv")
 predict_ts<-function(temp, songname, days, conflow, confhigh){
     n=ncol(temp)-which(names(temp)=="Day.1")
@@ -149,11 +237,6 @@ ui <- navbarPage(title = "Final Project of Stat 427",
                               "Max","Min","Mean","Median","PMM"
                            )),
         
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
             
             downloadButton("downloadData", "Download")
             ),
@@ -171,17 +254,60 @@ ui <- navbarPage(title = "Final Project of Stat 427",
                      sidebarPanel(
                          # Enter URL or choose from existing datasets
                          
-                         sliderInput("days", "how many days forward you want to predict",
+                         sliderInput("days", "How many days forward you want to predict",
                                      min = 1, max = 15, value = 5
                          ),
                          textInput("songname", "Which song you want to predict? Please correctly spell the song name:"),
                          
-                         h4("The song name can be Kraazy, Rat, Adderall (Corvette Corvette), R60LLXN N CONTROLLXN, 	YKWIM, etc.", style = "font-family:Times"),
+                         h5("The song name can be Kraazy, Rat, Adderall (Corvette Corvette), R60LLXN N CONTROLLXN, 	YKWIM, etc.", style = "font-family:Times"),
                          actionButton("Submit" , label = "Submit", value="Krazzy")
                      ),
+              
                      mainPanel(
                          plotOutput("timeseries")
                      )
+                 )
+                 
+             )
+    ),
+    tabPanel(title = "Cluster and XGBoost",
+             fluidPage(
+                 h1("NCS-based clustering and XGBoost-based prediction", style = "font-family:Times"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         tags$head(tags$style("#result_xgboost{color: grey;
+                         font-size: 30px;
+                         font-style: italic;
+                         }"
+                         )
+                         ),
+                         
+                         textInput("url_xgboost", "Please input the url for the song you want to predict:",value = "https://raw.githubusercontent.com/Zhenz2020/Stat427-Project/main/test_data.csv"),
+                         
+                         selectizeInput("num_cluster",
+                                        label = "How many clusters you want to set",
+                                        selected = 5,
+                                        choices = c(
+                                            1,2,3,4,5,6,7,8
+                                        )),
+                         h5("For this data set, the best number of clusters is 5", style = "font-family:Times"),
+                         
+                         sliderInput("days_xgboost", "How many days you want to keep",
+                                     min = 1, max = 30, value = 15
+                         ),
+                         
+                         
+                         actionButton("Submit_cluster" , label = "Submit", value="Krazzy")
+                     ),
+                     mainPanel(
+                         plotOutput("cluster"),
+                         plotOutput("xgboost"),
+                         textOutput("result_xgboost")
+                         
+                         
+                         
+                     )
+
                  )
                  
              )
@@ -271,6 +397,41 @@ server <- function(input, output) {
             read_csv("https://raw.githubusercontent.com/Zhenz2020/Stat427-Project/main/New_GeniusPageView.csv")
         })
         predict_ts(temp(),input$songname,input$days,50,60)
+    })
+    
+    
+    glob <- reactive({
+        temp=read.csv("https://raw.githubusercontent.com/Zhenz2020/Stat427-Project/main/train_data.csv")
+        cluster_tstemp(temp,as.integer(input$num_cluster))
+    })
+    
+    output$cluster <- renderPlot({
+        
+        temp=read.csv("https://raw.githubusercontent.com/Zhenz2020/Stat427-Project/main/train_data.csv")
+        cluster_plot(temp,as.integer(input$num_cluster))
+    })
+    
+    
+    output$xgboost <- renderPlot({
+        
+        temp1=eventReactive(input$Submit_cluster, {
+            read.csv(input$url_xgboost)
+        })
+        #ts_temp_t=glob()
+        #text_xgb=xgboost_pre(as.matrix(temp1()),ts_temp_t,as.integer(input$days_xgboost))
+        plot(as.matrix(temp1(),nrow = 1)[1:as.integer(input$days_xgboost)],col=1,type = "l",lty=1,main="Scatter plot of the input data set",ylab="Values",xlim=c(1,31),ylim=c(-3,3))
+        abline(v=as.integer(input$days_xgboost),col="red",lty=2,lwd=3)
+        lines(x=c(as.integer(input$days_xgboost):31),y=as.matrix(temp1(),nrow = 1)[(as.integer(input$days_xgboost)):31],col=2,lty=1)
+        legend("topleft", legend=c("Observed Data","Unobserved real variables"),col=c(1,2),lty=c(1,1), cex=0.8)
+
+    })
+    
+    output$result_xgboost <- renderText({ 
+        temp2=eventReactive(input$Submit_cluster, {
+            read.csv(input$url_xgboost)
+        })
+        ts_temp_t=glob()
+        xgboost_pre(temp2(),ts_temp_t,as.integer(input$days_xgboost))
     })
 }
 
